4135a4ada4599f378271eda2920eef7ba0d5b900
Sharron J <sj7723@aol.com>
2017-02-03 15:07:28 -0500

    first commit

---
+++ src/Creatingclass.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Owner on 2/3/2017.
+ * creating methods project
+ */
+
+         //1 created a main using psvm short cut in class body
+            public static void main(String) args [] {
+            test1();
+            test2( age:16);
+            int price = test3();
+            System.out.println(test)());test3(1);
+        }
+        //2.create another method called test1 returns void, no paremeters
+        public static void test1 (){
+
+        }
+
+        //3.create another method called test2 returns void, 1 int parameter
+        public static void test2 (int age){
+        int=test2;
+        age = 21
+        System.out.println(age);
+        }
+
+        //4create another method called test3 returns int, no parameters
+        //when you see no parameters this means no brackets
+        public static int test3(){return 1;}
+
+        //4. create another method called test4 returns float, 1 scanner class parameter, no parameters
+        public static float test4(Scanner input){
+        return 3.14f;
+
+        //6. Create another method called test5 returns a String, 3 float parameters
+        public static String test5(float param1,float param2,float param3){
+        return"Hi";
+
+        }
+
